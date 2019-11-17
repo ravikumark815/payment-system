6718ec2ed0c3978836fdc65864951143aa022eb7
Ravi Kumar Reddy K <RaviKumarReddyK@sonicwall.com>
2019-11-17 10:44:01 +0530

    Added choices, viewing fees, paying fees and so on

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import time
 import pymysql as sql
 import regex as re
 import inspect
+import datetime
 
 gVerbose = 0
+gLoggedIn = 0
 
 # Print Line Number
 def lineno():
@@ -19,6 +21,23 @@ def choice(db, cursor):
     elif choice=="2":
         register(db, cursor)
     elif choice=="3":
+        print("\n---- Thank You! ----\n")
+        gLoggedIn = 0
+        db.close()
+        exit()
+    else:
+        print("Enter a Valid Choice\n")
+
+def choice2(db, cursor, uname):
+    choice = input("\n1. View Current Fee Status\n2. Pay Fees\n3. View Payments\n4. Logout\n\n")
+    if choice=="1":
+        view_fees(db, cursor, uname)
+    elif choice=="2":
+        pay_fees(db, cursor, uname)
+    elif choice=="3":
+        view_fees(db, cursor, uname)
+    elif choice=="4":
+        gLoggedIn = 0
         db.close() 
         exit()
     else:
@@ -66,7 +85,8 @@ def login(db, cursor):
             if pwd == dbpwd[0]:
                 time.sleep(1)
                 print("\n>> Login Successful\n")
-                view_fees(db, cursor, uname)
+                gLoggedIn = 1
+                choice2(db, cursor, uname)
             else:
                 time.sleep(1)
                 print("\n>> Password Incorrect\n")
@@ -171,7 +191,73 @@ def view_fees(db, cursor, uname):
     cursor.execute(sqlquery)
     dbfees = cursor.fetchone()
     print("\nStationary Fees:", dbfees)
-    choice(db, cursor)
+    choice2(db, cursor, uname)
+
+def pay_fees(db, cursor, uname):
+    if(gLoggedIn):
+        print("\n--- Pay Fees ---\n")
+        choice = input("\n1. Exam Fees\n2. Lab Fees\n3. Placement Fees\n4. Library Fees\n5. Stationary Fee\n6. Club Fees\n\n7.Logout\n\n")
+        if choice=="1":
+            print("\n--- Exam Fees ---\n")
+            fee_type = "exam_fee"
+        elif choice=="2":
+            print("\n--- Lab Fees ---\n")
+            fee_type = "lab_fee"
+        elif choice=="3":
+            print("\n--- Placement Fees ---\n")
+            fee_type = "placement_fee"
+        elif choice=="4":
+            print("\n--- Library Fees ---\n")
+            fee_type = "library_fee"
+        elif choice=="5":
+            print("\n--- Stationary Fees ---\n")
+            fee_type = "stationary_fee"
+        elif choice=="6":
+            print("\n--- Club Fees ---\n")
+            fee_type = "club_fee"
+        elif choice=="7":
+            gLoggedIn = 0
+            db.close() 
+            exit()
+        
+        sqlquery = "SELECT"+str(fee_type)+"FROM fps.fees where uname=\""+str(uname)+"\";"
+        if gVerbose:
+            print("\n*** Debug ["+str(lineno())+"]:\t"+sqlquery+"\t***\n")
+        cursor.execute(sqlquery)
+        dbfees = cursor.fetchone()
+        print("\nCurrent Balance:", dbfees)
+        amount = input("\nEnter amount to be paid:")
+        while(int(amount) <= 0):
+            amount = input("\nEnter valid amount:")
+        card_number = input("\nEnter your Credit/Debit Card number:")
+        while not(re.match(r"\d{16}",str(card_number), re.I|re.M)):
+            card_number = input("\nEnter valid Credit/Debit Card number:")
+        cvv = input("\nEnter your 3 digit CVV number:")
+        while not(re.match(r"\d{3}",str(cvv), re.I|re.M)):
+            cvv = input("\nEnter valid CVV number:")
+        expiry_date = input("\nEnter expiry date of your card:")
+        while not(re.match(r"\d{0,2}\/\d{4}",str(expiry_date), re.I|re.M)):
+            expiry_date = input("\nEnter a valid expiry date:")
+        now = str(datetime.datetime.now())
+        print("\nDo Not Refresh/Cancel while the transaction is being processed\n")
+        time.sleep(3)
+        trans_id = now.strip()
+        trans_id = trans_id.replace("-","")
+        trans_id = trans_id.replace(":","")
+        trans_id = trans_id.replace(".","")
+        transaction_id = trans_id[::-1]
+        print("\ntransaction_id: ",transaction_id)
+        if gVerbose:
+            print("\ntransaction_id: ",transaction_id)
+        sqlquery = "INSERT INTO fps.login (uname, type, amount_to_be_paid, amount_paid, amount_left, card_number, cvv, expiry_date, time_stamp, transaction_id) VALUES(\""+uname+"\", \""+fee_type+"\", \""+dbfees+"\", \""+amount+"\", \""+(int(dbfees)-int(amount))+"\", \""+card_number+"\", \""+cvv+"\", \""+expiry_date+"\", \""+str(now)+"\", \""+transaction_id+"\");"
+        if gVerbose:
+            print("\n*** Debug ["+str(lineno())+"]:\t"+sqlquery+"\t***\n")
+        cursor.execute(sqlquery)
+        cursor.execute("COMMIT;")
+        print("\n>Transaction Successful\n")
+        choice2(db, cursor, uname)
+    else:
+        choice(db, cursor)
 
 if __name__ == "__main__": 
     print("\n\n-------- Welcome to Fee Payment System! --------\n")
