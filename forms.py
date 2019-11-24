c4f1bdd2797b8193d4583080e8d13745c493505e
Ravi Kumar Reddy K <RaviKumarReddyK@sonicwall.com>
2019-11-24 16:10:57 +0530

    Login_Manager

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from main import Login
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -9,6 +10,11 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm_Password', 
                 validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_uname(self, uname):
+        user = Login.query.filter_by(uname=uname.data).first()
+        if user:
+            raise ValidationError('Username is already registered. Please login with the same.')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', 
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
+# Libraries
 import fps
 from datetime import datetime
-from forms import RegistrationForm, LoginForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, flash, redirect
-app = Flask(__name__)
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, current_user, UserMixin, login_user, logout_user
 
+# Extensions
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b6d0232afb913ea0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/fps'
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
-class Login(db.Model):
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                validators=[DataRequired(), Length(min=10, max=10)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm_Password', 
+                validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_uname(self, uname):
+        user = Login.query.filter_by(uname=uname.data).first()
+        if user:
+            raise ValidationError('Username is already registered. Please login with the same.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', 
+                validators=[DataRequired(), Length(min=10, max=10)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class Login(db.Model, UserMixin):
     uname = db.Column(db.String(10), primary_key=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
@@ -45,28 +72,53 @@ class Payment(db.Model):
         return f"Login('{self.trans_id}', '{self.uname}', '{self.amount_to_be_paid}', '{self.amount_paid}', '{self.amount_left}', '{self.card_number}', '{self.cvv}', '{self.expiry_date}', '{self.time_stamp}')"
 
 @app.route("/")
+def default():
+    return render_template('default.html')
+
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Login.query.get(user_id)
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('login'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = Login.query.filter_by(uname=form.username.data).first()
+        if user:
+            flash('Username is already registered', 'danger')
+        else:
+            user = Login(uname=form.username.data, password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Your account has been created', 'success')
+            return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.password.data == 'password':
-            flash('Login Successful', 'success')
+        user = Login.query.filter_by(uname=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please verify username and password','danger')
     return render_template('login.html', title='login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('default'))
 
 if __name__ == "__main__": 
     app.run(debug=True)
